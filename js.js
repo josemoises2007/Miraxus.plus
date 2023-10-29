d8447e2e1b28f85e7709f2bc1625965bb3cf2c80
jose moises <106701602+josemoises2007@users.noreply.github.com>
2023-10-29 14:03:29 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,20 +1,22 @@
-var audio = document.getElementById('audio');
-var audioButton = document.getElementById('audioButton');
-var audioImage = document.getElementById('audioImage');
-audioButton.addEventListener('click', function() {
-    if (audio.paused) {
-        audio.play();
-        audioButton.textContent = 'stop'; // Cambiar el texto del botón
-        audioImage.src = 'https://github.com/josemoises2007/h/assets/106701602/1160db46-1eec-4d51-8197-8b86aed71b7a'; // Cambiar la imagen a pausa
-    } else {
-        audio.pause();
-        audioButton.textContent = 'Play'; // Cambiar el texto del botón
-        audioImage.src = 'https://github.com/josemoises2007/h/assets/106701602/3fe155bb-0701-4074-9f17-23f0d2c73f2b'; // Cambiar la imagen a reproducción
-    }
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var audio = document.getElementById('audio');
+    var audioButton = document.getElementById('audioButton');
+    var audioImage = document.getElementById('audioImage');
 
-audio.addEventListener('ended', function() {
-    audio.play();
-    audioButton.textContent = 'Stop'; // Restaurar el texto del botón
-    audioImage.src = 'https://github.com/josemoises2007/h/assets/106701602/1160db46-1eec-4d51-8197-8b86aed71b7a'; // Restaurar la imagen a pausa al finalizar la canción
+    audioButton.addEventListener('click', function() {
+        if (audio.paused) {
+            audio.play();
+            audioButton.textContent = 'Stop'; // Altera o texto do botão
+            audioImage.src = 'https://github.com/josemoises2007/h/assets/106701602/1160db46-1eec-4d51-8197-8b86aed71b7a'; // Altera a imagem para pausa
+        } else {
+            audio.pause();
+            audioButton.textContent = 'Play'; // Altera o texto do botão
+            audioImage.src = 'https://github.com/josemoises2007/h/assets/106701602/3fe155bb-0701-4074-9f17-23f0d2c73f2b'; // Altera a imagem para reprodução
+        }
+    });
+
+    audio.addEventListener('ended', function() {
+        audioButton.textContent = 'Play'; // Restaura o texto do botão
+        audioImage.src = 'https://github.com/josemoises2007/h/assets/106701602/1160db46-1eec-4d51-8197-8b86aed71b7a'; // Restaura a imagem para reprodução ao finalizar a música
+    });
 });
